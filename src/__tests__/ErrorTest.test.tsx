b0bc216b18946029436d001e286be506affa341f
Maya Zakharova <may@Mayas-MacBook-Pro.local>
2025-02-23 08:48:50 -0600

    Updates

--- src/__tests__/ErrorTest.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import ErrorTest from '../components/ErrorTest';
-describe('ErrorTest Component', () => {
-  test('should throw an error', () => {
-    expect(() => render(<ErrorTest />)).toThrow(
-      'This is a test error from ErrorTest!'
-    );
-  });
-});
--- src/components/ErrorTest.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ErrorTest: React.FC = () => {
-  throw new Error('This is a test error from ErrorTest!');
-};
-
-export default ErrorTest;
